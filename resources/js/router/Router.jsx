1758e51a0b1de2b7348e1cc7b7e8500d62836619
FekalisPoganchik <igorlebedev018@gmail.com>
2023-03-05 13:41:50 +0500

    app

---
+++ resources/js/router/Router.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import IndexProduct from "../components/products/IndexProduct";
+import NewProduct from "../components/products/NewProduct";
+import EditProduct from "../components/products/EditProduct";
+
+import NotFound from "../components/NotFound";
+
+const Router = () => {
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<IndexProduct />} />
+                <Route path="/product/new" element={<NewProduct />} />
+                <Route path="/product/edit/:id" element={<EditProduct />} />
+                <Route path="/*" element={<NotFound />} />
+            </Routes>
+        </div>
+    );
+};
+
+export default Router;
